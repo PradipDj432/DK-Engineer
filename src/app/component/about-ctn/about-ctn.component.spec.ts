34fa3319f5738481874f46889afbcd55223e12d7
WIN-S5UJ3BO1L7K\Pradip_Optimum <pradip.j@optimumfintech.co.in>
2024-03-14 07:53:49 +0000

    Error Resolve

--- src/app/component/about-ctn/about-ctn.component.spec.ts
+++ src/app/component/about-ctn/about-ctn.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AboutCtnComponent } from './contact-ctn.component';
+import { AboutCtnComponent } from './about-ctn.component';
 
 describe('AboutCtnComponent', () => {
   let component: AboutCtnComponent;
