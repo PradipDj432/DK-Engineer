e432fd5c00f0dd90f9c85d7d51c9bd4ac3ba8661
Sumit-Bhojani <128684621+Sumit-Bhojani@users.noreply.github.com>
2024-05-02 23:16:48 +0530

    Contact Page Map Redirect and CSS

--- src/app/component/contact/contact.component.ts
+++ src/app/component/contact/contact.component.ts
@@ -11,5 +11,11 @@ export class ContactComponent {
   ngOnInit() {
     this.viewportScroller.scrollToPosition([0, 0]);
   }
-  
+  RedirectToLocation() {
+    debugger
+    //let latitude ="20.3686976";
+    //let longitude ="72.9217278";
+    const googleMapsUrl = `https://www.google.com/maps/place/DK+ENGINEER'S/@20.3687117,72.9058486,15z/data=!3m1!4b1!4m6!3m5!1s0x3be0cfaa9d16867f:0xcd7aa57fc58c925b!8m2!3d20.3686926!4d72.9243027!16s%2Fg%2F11khmv3jxh?entry=ttu`;
+    window.open(googleMapsUrl, '_blank');
+  }
 }
